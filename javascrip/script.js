57eaa01315f1487dc94768c0e887dea1a3d69172
Beccix <rennato.sg@gmail.com>
2021-03-25 13:55:19 -0300

    refis o layout todo

--- javascrip/script.js
+++ javascrip/script.js
@@ -1,5 +1,14 @@
 function HideShow() {
-    const element = document.getElementById('p-target');
+    const elementFirst = document.getElementById('p-target');
+    if (elementFirst.classList.contains('hide')) {
+        elementFirst.classList.remove('hide')
+        elementFirst.classList.add('show')
+    } else {
+        elementFirst.classList.add('hide')
+        elementFirst.classList.remove('show')
+    }
+
+    const elementSecond = document.getElementById('p-target');
     if (element.classList.contains('hide')) {
         element.classList.remove('hide')
         element.classList.add('show')
