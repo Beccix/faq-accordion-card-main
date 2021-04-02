e57de7e6005de855b1dc621b6081ca2971cec823
Beccix <rennato.sg@gmail.com>
2021-04-02 03:24:44 -0300

    add arrow bg

--- javascrip/script.js
+++ javascrip/script.js
@@ -3,13 +3,14 @@ let accBody = document.getElementsByClassName('accordion-button');
 
 for (i = 0; i < accBody.length; i++) {
     accBody[i].addEventListener('click', toggleItem, false)
-}
+    
+} 
     function toggleItem() {
         let itemClass = this.parentNode.className;
         for (let i = 0; i < accItem.length; i++) {
             accItem[i].className = 'accordion-item hide';
         }
         if (itemClass === 'accordion-item hide') {
-            this.parentNode.className = 'accordion-item show';
+            this.parentNode.className = 'accordion-item show active';
         }
     }
